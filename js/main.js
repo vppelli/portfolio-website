9dcfff275ee576f9e6a1c680c68b2fae200e2018
Mikey P <138417934+vppelli@users.noreply.github.com>
2024-03-13 22:10:24 -0400

    Updated index.html Added imgs
    Activated the Art section in index.html
    Updated styles sheet
    Added images for all art projects
    Added Medium socials

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
-let scrollPos=0;const fullSideBar=document.getElementById("full"),miniSideBar=document.getElementById("mini");let showSidebar=()=>{fullSideBar.classList.remove("is-hidden"),miniSideBar.classList.add("is-hidden")},hideSidebar=()=>{fullSideBar.classList.add("is-hidden"),miniSideBar.classList.remove("is-hidden")};
+let scrollPos = 0; 
+const fullSideBar = document.getElementById("full"); 
+const miniSideBar = document.getElementById("mini"); 
+const code = document.getElementById("code");
+const art = document.getElementById("art");
+const artBtn = document.getElementById("artbtn");
+const codeBtn = document.getElementById("codebtn");
+const codefont = document.getElementById("codefont");
+const artfont = document.getElementById("artfont");
 
+let showSidebar = () => {
+    fullSideBar.classList.remove("is-hidden"); 
+    miniSideBar.classList.add("is-hidden");
+}
+let hideSidebar = () => {
+    fullSideBar.classList.add("is-hidden"); 
+    miniSideBar.classList.remove("is-hidden");
+};
+
+let showArt = () => {
+    art.classList.remove("hide");
+    code.classList.add("hide");
+    artBtn.classList.add("current-layout");
+    codeBtn.classList.remove("current-layout");
+    artfont.classList.remove("font-position");
+    codefont.classList.add("font-position");
+}
+let showCode = () => {
+    art.classList.add("hide");
+    code.classList.remove("hide");
+    artBtn.classList.remove("current-layout");
+    codeBtn.classList.add("current-layout");
+    artfont.classList.add("font-position");
+    codefont.classList.remove("font-position");
+}
