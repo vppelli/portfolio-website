6971104b230a338f1e5553419c747c13d3666cd9
Mikey P <138417934+vppelli@users.noreply.github.com>
2024-03-19 19:47:05 -0400

    Updated CSS, JS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,22 @@
-// let scrollPos = 0;
+let scrollPos = 0;
 const fullSideBar = document.getElementById("full");
 const miniSideBar = document.getElementById("mini");
-const fullInner = document.getElementById("full-inner");
-const miniInner = document.getElementById("mini-inner");
 const code = document.getElementById("code");
 const art = document.getElementById("art");
 const artBtn = document.getElementById("artbtn");
 const codeBtn = document.getElementById("codebtn");
 const codefont = document.getElementById("codefont");
 const artfont = document.getElementById("artfont");
-// const nav = document.querySelector('.flex-layout');
+const nav = document.querySelector('.flex-layout');
 
 let showSidebar = () => {
     fullSideBar.classList.remove("is-hidden");
     miniSideBar.classList.add("is-hidden");
-    miniInner.classList.add("hide");
-    fullInner.classList.remove("hide");
 
 }
 let hideSidebar = () => {
     fullSideBar.classList.add("is-hidden");
     miniSideBar.classList.remove("is-hidden");
-    fullInner.classList.add("hide");
-    miniInner.classList.remove("hide");
 };
 
 let showArt = () => {
@@ -42,31 +36,31 @@ let showCode = () => {
     codefont.classList.remove("font-position");
 }
 
-// let checkPosition = () => {
-//     let windowY = window.scrollY;
-//     if (windowY < scrollPos) {
-//         // Scrolling UP
-//         hideSidebar();
-//     } else {
-//         // Scrolling DOWN
-//         hideSidebar();
-//     }
-//     scrollPos = windowY;
-// }
+let checkPosition = () => {
+    let windowY = window.scrollY;
+    if (windowY < scrollPos) {
+        // Scrolling UP
+        hideSidebar();
+    } else {
+        // Scrolling DOWN
+        hideSidebar();
+    }
+    scrollPos = windowY;
+}
 
-// function debounce(func, wait = 10, immediate = true) {
-//     let timeout;
-//     return function () {
-//         let context = this, args = arguments;
-//         let later = function () {
-//             timeout = null;
-//             if (!immediate) func.apply(context, args);
-//         };
-//         let callNow = immediate && !timeout;
-//         clearTimeout(timeout);
-//         timeout = setTimeout(later, wait);
-//         if (callNow) func.apply(context, args);
-//     };
-// };
+function debounce(func, wait = 10, immediate = true) {
+    let timeout;
+    return function () {
+        let context = this, args = arguments;
+        let later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
-// window.addEventListener('scroll', debounce(checkPosition));
+window.addEventListener('scroll', debounce(checkPosition));
